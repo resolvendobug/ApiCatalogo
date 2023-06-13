cabc860de25cb1bc615be64fc96c05baeff00b0e
Renato Carvalho <renato@resolvendobug.com.br>
2023-06-13 17:15:12 -0300

    Add Api.UnitTest

---
+++ Api.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Api.Models;
+
+namespace Api.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+       // Categoria cat; 
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+       // Categoria cat; 
+        Assert.True(true);
+    }
+}
